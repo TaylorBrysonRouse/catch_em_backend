c18de01d7aeb8885086ebff8dbfa2e869d7feb57
TaylorBrysonRouse <taylorbrysonrouse@gmail.com>
2022-10-27 00:01:44 -0400

    Add modeling for cloud types

---
+++ backend/db/models/cloud_types.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+from db.base_class import Base
+
+class CloudType(Base):
+  id = Column(Integer, primary_key = True, index = True)
+  cloud_type_name = Column(String, nullable = False)
+  cloud_type_range_min = Column(Integer, nullable = False)
+  cloud_type_range_max = Column(Integer, nullable = False)
+
+  catches = relationship("Catch", back_populates = "cloud_coverage")
