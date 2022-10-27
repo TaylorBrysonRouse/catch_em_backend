a7baea40d12fbe5474693cb2fed0ac88dab648e3
TaylorBrysonRouse <taylorbrysonrouse@gmail.com>
2022-10-27 00:10:00 -0400

    Add modeling for baits

--- backend/db/models/baits.py
+++ backend/db/models/baits.py
@@ -2,3 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
 from db.base_class import Base
+
+class Bait(Base):
+  id = Column(Integer, primary_key = True, index = True)
+  bait_name = Column(String, nullable = False)
