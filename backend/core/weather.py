b02c77fbe818cad53893d1fbfe8d07b5fbdd9413
TaylorBrysonRouse <taylorbrysonrouse@gmail.com>
2022-10-31 21:42:41 -0400

    Change method name for Weather API

--- backend/core/weather.py
+++ backend/core/weather.py
@@ -6,7 +6,7 @@ OPEN_WEATHER_MAP_API_KEY = settings.OPEN_WEATHER_MAP_API_KEY
 
 OPEN_WEATHER_MAP_URL = "https://history.openweathermap.org/data/3.0/history/timemachine?"
 
-def get_weather_conditions(lat, long, time):
+def GetWeatherConditions(lat, long, time):
   unix_time = datetime.datetime(time.year, time.month, time.day, time.hour, time.minute, time.secound).timestamp()
 
   open_weather_map_get_request = f"{OPEN_WEATHER_MAP_URL}lat={lat}&long={long}&dt={unix_time}&appid={OPEN_WEATHER_MAP_API_KEY}"
