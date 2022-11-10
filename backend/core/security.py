09b257cda95ebb1abd12650b8ad8944c4e1e0e5b
TaylorBrysonRouse <taylorbrysonrouse@gmail.com>
2022-11-09 23:34:51 -0500

    Add JWT Encoding

---
+++ backend/core/security.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import jwt, JWTError
+
+from core.config import settings
+
+def create_access_token(data: dict, expiration: Optional[timedelta] = None):
+  to_encode = data.copy()
+  if expiration:
+    expire = datetime.utcnow() + expiration
+  else:
+    expire = datetime.utcnow() + timedelta(minutes = settings.JWT_TOKEN_EXPIRATION_MINUTES)
+   
+  to_encode.update({"exp": expire})
+
+  encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET, algorithm = settings.JWT_ALGORITHM)
+  return encoded_jwt
